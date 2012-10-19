95a9408b4ebf4be57361585be864f70df2d709ad
crygiova <crygiova@gmail.com>
2012-10-19 18:19:00 +0200

    DEleting old authentication

--- src/swsec/Query.java
+++ src/swsec/Query.java
@@ -1,31 +1,8 @@
 package swsec;
 
-import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
 import java.sql.*;
 
 public class Query {	
-	
-	//TODO change to use Owasp
-	public static boolean loginAdminQuery(String usr,String passw) throws Exception
-	{
-		Integer count=0;
-		ResultSet rs = null;
-		Connection con = ConnectionDB.getConnection();
-		PreparedStatement stat=con.prepareStatement("SELECT * FROM admin_users WHERE  uname = ? AND pw = ?");
-		stat.setString(1,usr);
-		stat.setString(2, passw);
-		rs = stat.executeQuery();//making tha query
-		while (rs.next())
-		{
-			count++;
-		}
-		if(count==1)
-		{
-			return true;
-		}
-		return false;
-	}
 	
 	public static ResultSet SelectCountry()  throws Exception
 	{
